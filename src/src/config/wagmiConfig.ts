0ebcf0bdb09f1ef59d91b265c448d1ff50d065f6
itsumura-h <dumblepy@gmail.com>
2024-11-27 06:13:59 +0000

    fix

--- src/src/config/wagmiConfig.ts
+++ src/src/config/wagmiConfig.ts
@@ -1,9 +1,26 @@
 import { http, createConfig } from "wagmi";
 import { sepolia } from "wagmi/chains";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+import { WALLETCONNECT_PROJECT_ID } from "../config/env";
 
-export const config = createConfig({
-  chains: [sepolia],
-  transports: {
-    [sepolia.id]: http(),
-  },
-});
+export const wagmiConfig = createConfig(
+  getDefaultConfig({
+    // Your dApps chains
+    chains: [sepolia],
+    transports: {
+      // RPC URL for each chain
+      [sepolia.id]: http(sepolia.rpcUrls.default.http[0]),
+    },
+
+    // Required API Keys
+    walletConnectProjectId: WALLETCONNECT_PROJECT_ID,
+
+    // Required App Info
+    appName: "Romakin-testnet",
+
+    // Optional App Info
+    appDescription: "ConnectKit Wallet Connect Example",
+    appUrl: "https://reown.com/appkit", // your app's url
+    appIcon: "https://assets.reown.com/reown-profile-pic.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
+  }),
+);
--- src/src/index.tsx
+++ src/src/index.tsx
@@ -1,18 +1,20 @@
 import { LocationProvider, Router, Route, hydrate, prerender as ssr } from 'preact-iso';
+import { ConnectKitProviderWrapper } from './components/ConnectKitProvider';
 import { Home } from './pages/Home/index.jsx';
 import { NotFound } from './pages/_404.jsx';
-
 import './style.css';
 
 export function App() {
 	return (
 		<LocationProvider>
-			<main>
-				<Router>
-					<Home path="/" />
-					<NotFound default />
-				</Router>
+			<ConnectKitProviderWrapper>
+				<main>
+					<Router>
+						<Home path="/" />
+						<NotFound default />
+					</Router>
 				</main>
+			</ConnectKitProviderWrapper>
 		</LocationProvider>
 	);
 }
